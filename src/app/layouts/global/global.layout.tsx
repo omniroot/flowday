168cde71d0140bf352d390f3f28d511f64f0c081
omniroot <omnirootofc@gmail.com>
2025-02-06 21:44:22 +0300

    test

--- src/app/layouts/global/global.layout.tsx
+++ src/app/layouts/global/global.layout.tsx
@@ -2,24 +2,26 @@ import { Sidebar } from "@components/business/Sidebar/Sidebar.tsx";
 import { Outlet } from "@tanstack/react-router";
 import { useEffect } from "react";
 import styles from "./global.layout.module.css";
+import { ModernBottomNavigation } from "@components/business/ModernBottomNavigation/ModernBottomNavigation.tsx";
+import { Header } from "@ui/Header/Header.tsx";
 
 export const GlobalLayout = () => {
-	useEffect(() => {
-		const root = document.getElementById("root");
-		if (root) {
-			root.style.flexDirection = "row";
-		}
-	}, []);
+	// useEffect(() => {
+	// 	const root = document.getElementById("root");
+	// 	if (root) {
+	// 		root.style.flexDirection = "row";
+	// 	}
+	// }, []);
 
 	return (
 		<>
-			{/* <Header /> */}
+			<Header />
 			<Sidebar />
 			<main className={styles.main}>
 				<Outlet />
 			</main>
 			{/* <BottomNavigation /> */}
-			{/* <ModernBottomNavigation /> */}
+			<ModernBottomNavigation />
 			{/* <SearchLayout /> */}
 		</>
 	);
