7a14874ee37206fdcf8925089354923ff0738c0e
omniroot <omnirootofc@gmail.com>
2025-02-12 21:03:40 +0300

    Update project create

---
+++ src/features/projects/api/createProject/createProject.api.ts
@@ -0,0 +1,17 @@
+import { api } from "@/shared/api/api.ts";
+import { IProject } from "@features/projects/types/project.types.ts";
+import { createMutation } from "react-query-kit";
+
+type ICreateProject = Omit<IProject, "id">;
+
+const createProject = (project: ICreateProject) => {
+	return api.post<IProject>(`projects`, { id: 0, ...project });
+};
+
+export const useCreateProject = createMutation<IProject, ICreateProject>({
+	mutationKey: ["createProject"],
+	mutationFn: async (project) => {
+		const response = await createProject(project);
+		return response.data;
+	},
+});
